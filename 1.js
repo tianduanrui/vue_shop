1840392f714a1ca71e4c91ea6c28ab1942bcce33
tianduanrui <tianduanru1@163.com>
2021-10-01 15:46:11 +0800

    vue_shop

---
+++ 1.js
@@ -0,0 +1,11 @@
+
+function reverse(head, n) {
+  const count = 0
+  if (head === null || head.next === null) { return head }
+  while (count < n) {
+
+  }
+  return newhead
+}
+const head = { val: 1, next: { val: 2, next: { val: 3, next: { val: 4, next: null } } } }
+console.log(reverse(head, 2))
